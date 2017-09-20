cd881c3413ce1f9498fe46deee49bf281b2f84f3
Gabriel Vanderlei <gabrielvanderlei10@hotmail.com>
2017-09-20 19:02:43 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,107 @@
+var rect = 0;
+var i = 0;
+var time = 1000;
+var playing = false;
+var animation;
+var score = 0;
+
+function anim() {
+    return setInterval(load, time);
+}
+
+$(function () {
+    animation = anim();
+});
+
+function load() {
+
+    $("<div>")
+        .attr({
+            "class": "rect",
+            "data-top": 0,
+            "data-left": window.innerWidth * Math.random()
+        })
+
+        .appendTo("#game")
+        .on("mouseover", function () {
+            $(this).remove();
+            if (playing == true) {
+                score++;
+            }
+        });
+
+        $(".rect")
+        .each(function () {
+            $(this)
+            .css({
+                "top": $(this).attr("data-top") + "px",
+                "left": $(this).attr("data-left") + "px"
+            })
+            .attr({
+                "data-top": Number($(this).attr("data-top")) + 100
+            })
+
+            if (playing == true) {
+                $("#gameBoard")
+                .css({ "border-bottom": "15px solid white" });
+
+                if ($(this).attr("data-top") >= window.innerHeight) {
+                    clearInterval(animation);
+                    $(".rect").remove();
+                    lose();
+                    return false;
+                }
+            }
+        });
+
+    i++;
+    $(".rect").css({ "transform": "scale(" + Math.sin(i) + ")" });
+
+    if (time > 50) { time = time * 0.999999;  $(".rect").css({ "transform": "scale(" + Math.sin(i) + ")" });}
+    if (time < 400) { document.body.style.filter = "hue-rotate(" + i * 0.01 + "deg)"; }
+
+    clearInterval(animation);
+    animation = setInterval(load, time);
+}
+
+function play(){
+    score = 0;
+    playing = true;
+    clearInterval(animation);
+    $("#loseBoard")
+    .css({ "display": "none", "opacity": 0 });
+
+    $("#mainBoard")
+    .css({ "opacity": 0, "z-index": -1 });
+
+    $("#gameBoard")
+    .css({ "overflow": "hidden" });
+
+    $(".rect").remove();
+    animation = anim();
+}
+
+function lose(){
+    playing = false;
+    $("#loseBoard")
+    .css({ "display": "block", "opacity": 1 });
+    
+    $("#gameBoard")
+    .css({ "overflow": "hidden", "z-index":-1 });
+
+    $("#score")
+    .html(score);
+}
+
+function menu(){
+    
+    $("#loseBoard")
+    .css({ "display": "none", "opacity": 0 });
+    
+    $("#mainBoard")
+    .css({ "opacity": 1, "z-index": 30 });
+
+    $("#gameBoard")
+    .css({ "overflow": "", "z-index":20, "border-bottom": "none" });
+    
+}

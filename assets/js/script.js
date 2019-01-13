7666a637d18fe30672f17af402ca0aecb26870f0
Gabriel Vanderlei <gabrielvanderlei10@hotmail.com>
2019-01-13 12:48:32 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,27 @@ var velocity = 100;
 var difficult = 0;
 var verify = 0;
 
+function onDeviceReady() {
+    document.removeEventListener('deviceready', onDeviceReady, false);
+      alert('Anúncios configurados.');
+    
+      // Set AdMobAds options:
+    admob.setOptions({
+      publisherId:          "NONE",                                    // Replace with your AdMob id (if you don't have any, set it to "NONE")
+      tappxIdiOS:           "NONE",            // Replace with your Tappx Id for iOS
+      tappxIdAndroid:       "pub-45642-android-4249",        // Replace with your Tappx Id for Android
+      tappxShare:           1                                          // Do not use lower tappxShare ratio if you have set publixherId to "NONE"
+    });
+
+    // Start showing banners (will show atomatically as autoShowBanner is set to true by default)
+    admob.createBannerView();
+
+    // Request interstitial (will present automatically as autoShowInterstitial is set to true by default)
+    admob.requestInterstitial();
+  }
+  
+document.addEventListener("deviceready", onDeviceReady, false);
+
 function anim() {
     return setInterval(load, time);
 }
@@ -18,27 +39,7 @@ function anim() {
 $(function () {
     animation = anim();
 });
-
- function onDeviceReady() {
-      document.removeEventListener('deviceready', onDeviceReady, false);
-        alert('Anúncios configurados.');
-      
-        // Set AdMobAds options:
-      admob.setOptions({
-        publisherId:          "NONE",                                    // Replace with your AdMob id (if you don't have any, set it to "NONE")
-        tappxIdiOS:           "NONE",            // Replace with your Tappx Id for iOS
-        tappxIdAndroid:       "pub-45642-android-4249",        // Replace with your Tappx Id for Android
-        tappxShare:           1                                          // Do not use lower tappxShare ratio if you have set publixherId to "NONE"
-      });
  
-      // Start showing banners (will show atomatically as autoShowBanner is set to true by default)
-      admob.createBannerView();
- 
-      // Request interstitial (will present automatically as autoShowInterstitial is set to true by default)
-      admob.requestInterstitial();
-    }
- 
-    document.addEventListener("deviceready", onDeviceReady, false);
 
 function load() {
 

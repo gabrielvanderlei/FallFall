990ca08191aa48050a86812d6f09f18a4282c969
Gabriel Vanderlei <gabrielvanderlei10@hotmail.com>
2019-01-15 17:21:21 -0300

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 var rect = 0;
 var i = 0;
-var time = 1300;
+var time = 1000;
 var playing = false;
 var animation;
 var score = 0;
 var changing = 0;
 var rotateSd = 0;
 var leftRandom = 0;
-var velocity = 100;
+var velocity = 300;
 var difficult = 0;
 var verify = 0;
 
@@ -18,7 +18,7 @@ function onDeviceReady() {
     
       // Set AdMobAds options:
     admob.setOptions({
-      publisherId:          "NONE",                                    // Replace with your AdMob id (if you don't have any, set it to "NONE")
+      publisherId:          "ca-app-pub-5383874547802825~6746145444",                                    // Replace with your AdMob id (if you don't have any, set it to "NONE")
       tappxIdiOS:           "NONE",            // Replace with your Tappx Id for iOS
       tappxIdAndroid:       "pub-45642-android-4249",        // Replace with your Tappx Id for Android
       tappxShare:           1                                          // Do not use lower tappxShare ratio if you have set publixherId to "NONE"

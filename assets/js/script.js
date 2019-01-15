768c04d337e341633ac91b59683cda9c6184354f
Gabriel Vanderlei <gabrielvanderlei10@hotmail.com>
2019-01-15 17:36:29 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ var score = 0;
 var changing = 0;
 var rotateSd = 0;
 var leftRandom = 0;
-var velocity = 300;
+var velocity = 400;
 var difficult = 0;
 var verify = 0;
 
@@ -21,7 +21,7 @@ function onDeviceReady() {
       publisherId:          "ca-app-pub-5383874547802825~6746145444",                                    // Replace with your AdMob id (if you don't have any, set it to "NONE")
       tappxIdiOS:           "NONE",            // Replace with your Tappx Id for iOS
       tappxIdAndroid:       "pub-45642-android-4249",        // Replace with your Tappx Id for Android
-      tappxShare:           1                                          // Do not use lower tappxShare ratio if you have set publixherId to "NONE"
+      tappxShare:           0                                       // Do not use lower tappxShare ratio if you have set publixherId to "NONE"
     });
 
     // Start showing banners (will show atomatically as autoShowBanner is set to true by default)
